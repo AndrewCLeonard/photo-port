27ecaf148d224d2064257d4aea23c2a5cc7a2d68
Andrew Leonard <AndrewCLeonard@fastmail.com>
2022-04-05 18:12:53 -0700

    WIP contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,18 +1,41 @@
+// mine
 import React, { useState } from "react";
+
+import { validateEmail } from "../../utils/helpers";
 
 function ContactForm() {
 	// manage form data & set initial values to be empty
 	const [formState, setFormState] = useState({ name: "", email: "", message: "" });
+
+	const [errorMessage, setErrorMessage] = useState("");
 	const { name, email, message } = formState;
 
-	function handleChange(e) {
-		setFormState({ ...formState, [e.target.name]: e.target.value });
-	}
-
-	function handleSubmit(e) {
+	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(formState);
-	}
+		if (!errorMessage) {
+			setFormState({ [e.target.name]: e.target.value });
+			console.log("Form", formState);
+		}
+	};
+
+	const handleChange = (e) => {
+		if (e.target.name === "email") {
+			const isValid = validateEmail(e.target.value);
+			console.log(isValid);
+			// isValid conditional statement
+			if (!isValid) {
+				setErrorMessage("Your email is invalid.");
+			} else {
+				setErrorMessage("");
+			}
+		} else {
+			if (!e.target.value.length) {
+				setErrorMessage(`${e.target.name} is required.`);
+			} else {
+				setErrorMessage("");
+			}
+		}
+	};
 
 	return (
 		<section>
@@ -20,18 +43,24 @@ function ContactForm() {
 			<form id='contact-form' onSubmit={handleSubmit}>
 				<div>
 					<label htmlFor='name'>Name:</label>
-					<input type='text' defaultValue={name} name='name' onChange={handleChange} />
+					<input type='text' name='name' defaultValue={name} onBlur={handleChange} />
 				</div>
 				<div>
 					<label htmlFor='email'>Email address:</label>
-					<input type='email' defaultValue={email} name='email' onChange={handleChange} />
+					<input type='email' defaultValue={email} name='email' onBlur={handleChange} />
 				</div>
 				<div>
 					<label htmlFor='message'>Message:</label>
-					<textarea rows='5' defaultValue={message} name='message' onChange={handleChange} />
+					<textarea name='message' rows='5' defaultValue={message} onBlur={handleChange} />
 				</div>
-
-				<button type='submit'>Submit</button>
+				{errorMessage && (
+					<div>
+						<p className='error-text'>{errorMessage}</p>
+					</div>
+				)}
+				<button data-testid='button' type='submit'>
+					Submit
+				</button>
 			</form>
 		</section>
 	);
